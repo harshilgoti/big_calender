5009d4866582e1f521e84b439e9d2d13dbed8540
harshil goti <harshilgoti@gmail.com>
2021-03-16 10:19:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import events from "./events";
+import { Calendar, Views, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import "react-big-calendar/lib/css/react-big-calendar.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(...args) {
+    super(...args);
+
+    this.state = { events };
+  }
+
+  handleSelect = ({ start, end }) => {
+    const title = window.prompt("New Event name");
+    if (title)
+      this.setState({
+        events: [
+          ...this.state.events,
+          {
+            start,
+            end,
+            title,
+          },
+        ],
+      });
+  };
+
+  render() {
+    return (
+      <>
+        <Calendar
+          style={{ height: "700px" }}
+          selectable
+          localizer={momentLocalizer(moment)}
+          events={this.state.events}
+          scrollToTime={new Date(1970, 1, 1, 6)}
+          defaultDate={new Date(2015, 3, 12)}
+          onSelectEvent={(event) => alert(event.title)}
+          onSelectSlot={this.handleSelect}
+          defaultView={Views.MONTHS}
+          step={30}
+          onNavigate={() => console.log("onNavigate")}
+          onView={() => console.log("onView")}
+          onRangeChange={() => console.log("onRangeChange")}
+        />
+      </>
+    );
+  }
 }
 
 export default App;
